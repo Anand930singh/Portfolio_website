75f488b2875e205538fbab653da07a81b3c55bfa
Anand930singh <anand2002king@gmail.com>
2024-06-30 10:16:12 +0530

    updated experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -33,7 +33,31 @@ function Experience() {
                         </div>
                     </div>
                     <div className='expDetailRight'>
-                        <img src='https://www.codeinbound.com/assets/images/main-logo/logo.png' alt='Not available' />
+                        <img src='https://www.codeinbound.com/assets/images/main-logo/logo.png' alt='Not available' height="60"/>
+                    </div>
+                </div>}
+            </div>
+            <div className='expCard'>
+                <div className='expHeadingCard'>
+                    <div className='companyName'>Software Developer Intern @ RIDE EVEE</div>
+                    <div className='timeDuration'>June 2024 - Present</div>
+                    <div className='openCloseIcon'>{active && ind===1?(<CiCircleMinus onClick={()=>handleExp(null)} size={35} style={{ background: 'transparent' }} />):(<CiCirclePlus onClick={()=>handleExp(1)} size={35} style={{ background: 'transparent' }} />)}
+                    </div>
+                </div>
+                {active&& ind===1&& <div className='expDetailCard'>
+                    <div className='expDetailLeft'>
+                        <div className='expLocation'>📍 Remote</div>
+                        <div className='expDetail'>
+                        I am responsible for developing the frontend UI, backend API, and integrating both for the Ride EVEE website dashboard.</div>
+                        <div className='techStacks'>
+                            <span>ReactJs</span>
+                            <span>MongoDB</span>
+                            <span>NodeJS</span>
+                            <span>ExpressJS</span>
+                        </div>
+                    </div>
+                    <div className='expDetailRight'>
+                        <img src='https://www.f6s.com/content-resource/profiles/3316299_th1.jpg' alt='Not available' />
                     </div>
                 </div>}
             </div>
@@ -41,10 +65,10 @@ function Experience() {
                 <div className='expHeadingCard'>
                     <div className='companyName'>Cricket Code Champions Hack</div>
                     <div className='timeDuration'>Nov</div>
-                    <div className='openCloseIcon'>{active&& ind===1?(<CiCircleMinus onClick={()=>handleExp(null)} size={35} style={{ background: 'transparent' }} />):(<CiCirclePlus onClick={()=>handleExp(1)} size={35} style={{ background: 'transparent' }} />)}
+                    <div className='openCloseIcon'>{active&& ind===2?(<CiCircleMinus onClick={()=>handleExp(null)} size={35} style={{ background: 'transparent' }} />):(<CiCirclePlus onClick={()=>handleExp(2)} size={35} style={{ background: 'transparent' }} />)}
                     </div>
                 </div>
-                {active &&ind===1&& <div className='expDetailCard'>
+                {active &&ind===2&& <div className='expDetailCard'>
                     <div className='expDetailLeft'>
                         <div className='expLocation'>📍 Online</div>
                         <div className='expDetail'>
